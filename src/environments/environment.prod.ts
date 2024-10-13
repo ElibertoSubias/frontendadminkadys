e5ad363e998f20e21d3a07a6b43d1116cc6797d2
Eliberto Subias <eliberto.subias.ortega@gmail.com>
2024-10-13 13:46:59 -0700

    Prueba

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  appBaseUrl: "http://213.210.13.30:3001/api",
-  appBaseUrlMedia: "http://213.210.13.30:3001/uploads",
+  appBaseUrl: "http://localhost:3001/api",
+  appBaseUrlMedia: "http://localhost:3001/uploads",
   configFile: "assets/settings/settings.json"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  appBaseUrl: "http://213.210.13.30:3001/api",
-  appBaseUrlMedia: "http://213.210.13.30:3001/uploads",
+  appBaseUrl: "http://localhost:3001/api",
+  appBaseUrlMedia: "http://localhost:3001/uploads",
   configFile: "assets/settings/settings.json"
 };
 
