ddb2000a788fa0edd41e31024af44d7cdfd4987f
Eliberto Subias <eliberto.subias.ortega@gmail.com>
2025-06-10 00:49:38 -0700

    fix bug

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  appBaseUrl: "http://localhost:3001/api",
-  appBaseUrlMedia: "http://localhost:3001/uploads",
+  appBaseUrl: "https://backendkadys.elisodevelopment.cloud/api",
+  appBaseUrlMedia: "https://backendkadys.elisodevelopment.cloud/uploads",
   configFile: "assets/settings/settings.json",
   // ... otras variables que ya tengas
   WARNING_TIME_BEFORE_EXPIRATION_MINUTES: 1, // Valor para desarrollo c
@@ -13,8 +13,8 @@ export const environment = {
   TOKEN_REFRESH_BUFFER_SECONDS: 15,
   TOKEN_KEY: 'jwt_token',
   REFRESH_TOKEN_DATA_KEY: 'refresh_token_data',
-  apiUrl: 'http://localhost:3001/api', // Ejemplo, ajusta tu URL de API
-  apiLogin: 'http://localhost:3001' 
+  apiUrl: 'https://backendkadys.elisodevelopment.cloud/api', // Ejemplo, ajusta tu URL de API
+  apiLogin: 'https://backendkadys.elisodevelopment.cloud' 
 };
 
 /*
