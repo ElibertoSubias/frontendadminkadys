e3e7dd0b7cfc24508c10dcfd5d30db32a4d3fcc4
Eliberto Subias <eliberto.subias.ortega@gmail.com>
2025-06-10 00:37:22 -0700

    test1

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,9 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
-  appBaseUrl: "https://backendkadys.elisodevelopment.cloud/api",
-  appBaseUrlMedia: "https://backendkadys.elisodevelopment.cloud/uploads",
+  production: false,
+  appBaseUrl: "http://localhost:3001/api",
+  appBaseUrlMedia: "http://localhost:3001/uploads",
   configFile: "assets/settings/settings.json",
   // ... otras variables que ya tengas
   WARNING_TIME_BEFORE_EXPIRATION_MINUTES: 15, // Valor para desarrollo
@@ -13,8 +13,8 @@ export const environment = {
   TOKEN_REFRESH_BUFFER_SECONDS: 15,
   TOKEN_KEY: 'jwt_token',
   REFRESH_TOKEN_DATA_KEY: 'refresh_token_data',
-  apiUrl: 'https://backendkadys.elisodevelopment.cloud/api', // Ejemplo, ajusta tu URL de API
-  apiLogin: 'https://backendkadys.elisodevelopment.cloud' 
+  apiUrl: 'http://localhost:3001/api', // Ejemplo, ajusta tu URL de API
+  apiLogin: 'http://localhost:3001' 
 };
 
 /*
